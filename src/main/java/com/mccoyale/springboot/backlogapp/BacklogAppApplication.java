59f03e34733878f8846be1e3f68d568d785e8c38
Alexandra McCoy <mccoyale@gmail.com>
2023-09-14 21:48:54 -0400

    spring boot initializer

---
+++ src/main/java/com/mccoyale/springboot/backlogapp/BacklogAppApplication.java
@@ -0,0 +1,13 @@
+package com.mccoyale.springboot.backlogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacklogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacklogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mccoyale/springboot/backlogapp/BacklogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mccoyale.springboot.backlogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacklogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
