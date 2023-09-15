ea966f92d9dda01489d585f50b91ef0a6726ab6d
Alexandra McCoy <mccoyale@gmail.com>
2023-09-14 22:55:24 -0400

    updated files and added security config, which requires editing still

--- src/main/java/com/mccoyale/springboot/backlogapp/backlogrepo.java
+++ src/main/java/com/mccoyale/springboot/backlogapp/backlogrepo.java
@@ -3,6 +3,6 @@ package com.mccoyale.springboot.backlogapp;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface backlogrepo extends JpaRepository < backlog, Long > {
+public interface backlogRepo extends JpaRepository < backlog, Long > {
     List < backlog > findByUserName(String user);
 }
